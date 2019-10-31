2e7a48dcd633879bdb8dc7a487eb17d00d1755b9
Fig77 <55416179+Fig77@users.noreply.github.com>
2019-10-31 12:54:11 -0300

    finished animation in the search box - nav bar of header

--- js/main.js
+++ js/main.js
@@ -1,21 +1,24 @@
 var transition = false;
 
-function setTransition() {
-  
-  if(transition == false ) {
-    var iconSearch = document.getElementById("search-icon");
-    var formChange = document.getElementById("search-bar-li"); 
+//All right
+
+function animateSearch() {
+  var iconSearch = document.getElementById("search-icon");
+  var formChange = document.getElementById("search-bar-li"); 
+  var inputBox = document.getElementById("form-nav-header");
+  if(transition == false) {
     iconSearch.style.color = "#707070";
-    formChange.style.width = "19%";
+    formChange.style.width = "25%";
+    inputBox.style.background = "rgba(255,255,255,1)"
     transition = true;
-    }
+  }
   else {
-    var iconSearch = document.getElementById("search-icon");
-    var formChange = document.getElementById("search-bar-li");
     iconSearch.style.color = "#fff";
     formChange.style.width = "15.5%";
+    inputBox.style.background = "rgba(255,255,255,0)"
     transition = false;
+   
   }
-  
 }
+
 

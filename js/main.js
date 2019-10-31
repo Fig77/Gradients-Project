83a5a7e0dac07527941eb23503c0916719a227f5
Fig77 <55416179+Fig77@users.noreply.github.com>
2019-10-30 21:27:36 -0300

    working on the animation for the nav bar

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function setTransition() {
+  var iconSearch = document.getElementById("search-icon");
+  var formChange = document.getElementById("search-bar-li"); 
+  iconSearch.style.color = "#707070";
+  formChange.style.width = "19%";
+}
+
+function setDefault() {
+  var iconSearch = document.getElementById("search-icon");
+  var formChange = document.getElementById("search-bar-li");
+  iconSearch.style.color = "#fff";
+  formChange.style.width = "12.5%";
+  
+}

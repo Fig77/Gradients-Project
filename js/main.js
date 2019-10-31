be6e1ccf2d21c493081c95cbf6b5a230e7fd1e31
Fig77 <55416179+Fig77@users.noreply.github.com>
2019-10-31 12:20:58 -0300

    changed transition function

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
-function setTransition() {
-  var iconSearch = document.getElementById("search-icon");
-  var formChange = document.getElementById("search-bar-li"); 
-  iconSearch.style.color = "#707070";
-  formChange.style.width = "19%";
-}
+var transition = false;
 
-function setDefault() {
-  var iconSearch = document.getElementById("search-icon");
-  var formChange = document.getElementById("search-bar-li");
-  iconSearch.style.color = "#fff";
-  formChange.style.width = "12.5%";
+function setTransition() {
+  
+  if(transition == false ) {
+    var iconSearch = document.getElementById("search-icon");
+    var formChange = document.getElementById("search-bar-li"); 
+    iconSearch.style.color = "#707070";
+    formChange.style.width = "19%";
+    transition = true;
+    }
+  else {
+    var iconSearch = document.getElementById("search-icon");
+    var formChange = document.getElementById("search-bar-li");
+    iconSearch.style.color = "#fff";
+    formChange.style.width = "15.5%";
+    transition = false;
+  }
   
 }
+
